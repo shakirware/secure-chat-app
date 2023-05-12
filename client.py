1f91a48b6915d0a173dbe541a72216b51522c844
Shakir <beefthesavage@protonmail.com>
2023-05-12 20:54:22 +0100

    TLS implemented.

--- client.py
+++ client.py
@@ -1,25 +1,30 @@
 import socket
-import threading 
+import ssl
+import threading
 
 class ChatClient:
-    def __init__(self, host, port):
+    def __init__(self, host, port, certfile):
         self.host = host
         self.port = port
+        self.certfile = certfile
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+        self.context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        self.context.load_verify_locations(self.certfile) # path to server's certificate
+        
     def connect(self):
+        self.socket = self.context.wrap_socket(self.socket, server_hostname=self.host)
         self.socket.connect((self.host, self.port))
-    
+        
     def send_message(self, message):
         self.socket.send(message.encode())
-    
+        
     def receive_messages(self):
         while True:
             message = self.socket.recv(1024).decode()
             if not message:
                 break
             print(message)
-    
+        
     def run(self):
         self.connect()
         print("Connected to server")
@@ -40,5 +45,6 @@ class ChatClient:
                 self.send_message(f"{username}: {message}")
                 
 if __name__ == '__main__':
-    client = ChatClient('localhost', 12100)
+    certfile = './certs/server.crt'
+    client = ChatClient('localhost', 12100, certfile)
     client.run()
--- server.py
+++ server.py
@@ -1,40 +1,48 @@
 import threading
 import socket
+import ssl
 
 class ChatServer(threading.Thread):
-    def __init__(self, host, port):
+    def __init__(self, host, port, certfile, keyfile):
         super().__init__()
         self.host = host
         self.port = port
+        self.certfile = certfile
+        self.keyfile = keyfile
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((self.host, self.port))
-        self.server_socket.listen(5)
+        self.ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        self.ssl_context.load_cert_chain(certfile=self.certfile, keyfile=self.keyfile)
+        self.server_socket_ssl = self.ssl_context.wrap_socket(self.server_socket, server_side=True)
+        self.server_socket_ssl.listen(5)
         self.clients = []
         
     def run(self):
         while True:
-            client_socket, client_address = self.server_socket.accept()
-            client_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
+            client_socket_ssl, client_address = self.server_socket_ssl.accept()
+            client_thread = threading.Thread(target=self.handle_client, args=(client_socket_ssl,))
             client_thread.start()
-            self.clients.append(client_socket)
+            self.clients.append(client_socket_ssl)
             
-    def handle_client(self, client_socket):
+    def handle_client(self, client_socket_ssl):
         while True:
             try:
-                message = client_socket.recv(1024).decode('utf-8')
-                self.broadcast(message, client_socket)
+                message = client_socket_ssl.recv(1024).decode('utf-8')
+                self.broadcast(message, client_socket_ssl)
             except:
-                self.clients.remove(client_socket)
+                self.clients.remove(client_socket_ssl)
                 break
                 
-    def broadcast(self, message, client_socket):
+    def broadcast(self, message, client_socket_ssl):
         for client in self.clients:
-            if client != client_socket:
+            if client != client_socket_ssl:
                 try:
                     client.send(message.encode('utf-8'))
                 except:
                     self.clients.remove(client)
                     
 if __name__ == '__main__':
-    server = ChatServer('localhost', 12100)
+    certfile = './certs/server.crt'
+    keyfile = './certs/server.key'
+    server = ChatServer('localhost', 12100, certfile, keyfile)
     server.start()
